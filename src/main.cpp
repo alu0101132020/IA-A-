742ad1ac259e522be966d6f5872f1d4f2622141b
Manuel Carrera <alu0101132020@ull.edu.es>
2019-10-28 23:33:50 +0000

    Se me olvidaron cosas jeje

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,12 @@ bool inspeccionar(nodo N, unsigned int fin){             //Se observa si el nodo
   return (N.get_ID() == fin);
 }
 
-int main(/*int argc, char **argv*/) {
-  // //if (argc == 2){
-  //
-    // std::string cadena = argv[1];
+int main(int argc, char **argv) {
+  if (argc == 3){
+    
+    std::string cadena = argv[1];
 
-    std::string cadena = "Grafo1.txt";
-
-    std::string heur = "Grafo1Heuristica1.txt";
+    std::string heur = argv[2];
 
     Arbol B(cadena);
 
@@ -20,26 +18,11 @@ int main(/*int argc, char **argv*/) {
 
 
     B.A_star_search(1, 8, heur);
-  //
-  // }
-  // else{
-  //   std::cout << "Introduzca un fichero válido." << std::endl;
-  // }
+
+  }
+  else{
+    std::cout << "Introduzca dos ficheros válido." << std::endl;
+  }
 
   return 0;
 }
-
-
-
-// nodo Ini(1, nullptr, 0, 0, 0);
-// std::list<nodo> Generados;
-// nodo sec(3, nullptr, 0, 0, 0);
-// nodo third(5, nullptr, 0, 0, 0);
-// Generados.push_back(Ini);
-// Generados.push_back(sec);
-// Generados.push_back(third);
-// while(!inspeccionar((*Generados.begin()), 4)){
-//   std::cout << "El nodo " << (*Generados.begin()).get_ID() << " va a ser destruido.";                //Compruebo si el nodo es el final, si no, genero sus suscesores.
-// Generados.erase(Generados.begin());
-//        }
-//        std::cout << "El nodo restante es " << (*Generados.begin()).get_ID() << "." << std::endl;
